cb3a5cedc57826481b1861fd67af6e600a1403a6
James Kim <jakim929@gmail.com>
2016-06-12 18:10:33 -0400

    Added new Schemas

---
+++ collections/musicProfile.js
@@ -0,0 +1,121 @@
+import { Mongo } from 'meteor/mongo';
+
+export const MusicProfiles = new Mongo.Collection('musicProfiles');
+
+OrchestraSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Orchestra Name"
+  },
+
+  position: {
+    type: String,
+    label: "Position in Orchestra"
+  },
+
+  startDate: {
+    type: Date,
+    label: "Beginning Date"
+  },
+
+  endDate: {
+    type: Date,
+    label: "Ending Date"
+  }
+});
+
+AwardSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Award Name"
+  },
+  date: {
+    type: Date,
+    label: "Date Received"
+  }
+});
+
+CompetitionSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Competition Name"
+  },
+  date: {
+    type: Date,
+    label: "Competition Date"
+  },
+  award: {
+    type: String,
+    label: "Award Type"
+  }
+});
+
+MusicProgramSchema = new SimpleSchema({
+  instName: {
+    type: String,
+    label: "Institution Name"
+  },
+
+  programName : {
+    type: String,
+    label: "Program Name"
+  },
+
+  startDate: {
+    type: Date,
+    label: "Start Date"
+  },
+
+  endDate: {
+    type: Date,
+    label: "End Date"
+  }
+})
+
+
+
+MusicProfileSchema = new SimpleSchema({
+  userId: {
+    type: String,
+    label: "User ID"
+  },
+
+  instrument: {
+    type: String,
+    label: "Instrument Played"
+  },
+
+  yearsPlayed: {
+    type: String,
+    label: "Years Played"
+  },
+
+  awards: {
+    type: [AwardSchema],
+    label: "Awards Won",
+    optional: true
+  },
+
+  competitions: {
+    type: [CompetitionSchema],
+    label: "Competitions Participated in",
+    optional: true
+  },
+
+  musicPrograms: {
+    type: [MusicProgramSchema],
+    label: "Music Programs Participated in",
+    optional: true
+  },
+
+  orchestras: {
+    type: [OrchestraSchema],
+    label: "Orchestras Played At",
+    optional: true
+  },
+
+
+
+});
+
+MusicProfiles.attachSchema(MusicProfileSchema)
