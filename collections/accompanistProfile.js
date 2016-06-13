c028e07eea4dad5fd1171d4019c36a31ea1b5dff
Abdulrahman Jamjoom <abdulrahmanjamjoom15@kingsacademy.edu.jo>
2016-06-13 11:09:11 -0400

    new accompanist still doesnt work

--- collections/accompanistProfile.js
+++ collections/accompanistProfile.js
@@ -2,11 +2,42 @@ import { Mongo } from 'meteor/mongo';
 
 export const AccompanistProfile = new Mongo.Collection('accompanistProfile');
 
-export const AccompanistProfileSchema = new SimpleSchema({
-  paymentType: {
-    type: String,
-    label: "Payment Type"
-  }
+AccompanistProfileSchema = new SimpleSchema({
+	repertoire: {
+    	type: [String],
+    	label: "Repertoire"
+  	},
+  	
+  	charge: {
+  		type: Number,
+  		label: "Hourly Charge",
+  		autoform: {
+      		options: [
+        	{label: "$20", value: 20},
+        	{label: "$40", value: 40},
+        	{label: "$60", value: 60}
+      		]
+   		}
+  	},
+
+  	location: {
+  		type: String,
+  		label: "Session's Location",
+  		autoform: {
+  			options: [
+        	{label: "My Place", value: "My Place"},
+        	{label: "Student's Place", value: "Student's Place"},
+        	{label: "Doesn't matter", value: "Doesn't matter"}
+      		]
+  		}
+  	},
+
+  	one_liner: {
+  		type: String,
+  		label: "Describe yourself in one sentence",
+  	},
+
 });
+
 
 AccompanistProfile.attachSchema(AccompanistProfileSchema)
