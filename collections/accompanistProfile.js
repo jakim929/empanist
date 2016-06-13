14900102e4bcbb35065e22c9e2d0788855cdb666
Abdulrahman Jamjoom <abdulrahmanjamjoom15@kingsacademy.edu.jo>
2016-06-13 12:18:08 -0400

    done with basic new accompanist signup page

--- collections/accompanistProfile.js
+++ collections/accompanistProfile.js
@@ -21,6 +21,38 @@ AccompanistProfileSchema = new SimpleSchema({
    		}
   	},
 
+  	working_hours: {
+  		type: [String],
+  		//noselect:true,
+  		label: "Times you prefer working",
+  		autoform: {
+  			type: "select-checkbox-inline",
+  			options: [
+        	{label: "Morning (8am - 12pm)", value: "morning"},
+        	{label: "Afternoon (12pm - 6pm)", value: "afternoon"},
+        	{label: "Night (6pm - 11pm)", value: "night"}
+      		]
+   		}
+  	},
+
+   	working_days: {
+  		type: [String],
+  		//noselect:true,
+  		label: "Days you want to accompany",
+  		autoform: {
+  			type: "select-checkbox-inline",
+  			options: [
+        	{label: "Monday", value: "monday"},
+        	{label: "Tuesday", value: "tuesday"},
+        	{label: "Wednesday", value: "wednesday"},
+        	{label: "Thursday", value: "thursday"},
+        	{label: "Friday", value: "friday"},
+        	{label: "Saturday", value: "saturday"},
+        	{label: "Sunday", value: "sunday"},
+      		]
+   		}
+  	}, 	
+
   	location: {
   		type: String,
   		label: "Session's Location",
