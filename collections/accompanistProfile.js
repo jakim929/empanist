94acc6064d97b072737e15df9e90b99e4d454b01
James Kim <jakim929@gmail.com>
2016-06-15 16:40:02 -0400

    Added accompanist information to the profile page

--- collections/accompanistProfile.js
+++ collections/accompanistProfile.js
@@ -26,16 +26,6 @@ AccompanistProfileSchema = new SimpleSchema({
     	label: "Repertoire"
   	},
 
-		dateBegin:{
-			type: Date,
-			label: "Begin Date"
-		},
-
-		dateEnd:{
-			type: Date,
-			label: "End Date"
-		},
-
   	charge: {
   		type: Number,
   		label: "Hourly Charge",
