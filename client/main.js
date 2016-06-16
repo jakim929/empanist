6e0309b3ae263593a9e61ea1eb17b7f1825b590c
James Kim <jakim929@gmail.com>
2016-06-16 16:51:45 -0400

    Added routes to check different profiles

--- client/main.js
+++ client/main.js
@@ -24,20 +24,39 @@ function wrapDoc (obj) {
 
 // Global Template Helpers
 
-Template.registerHelper( 'profileDoc', () => {
-    event.preventDefault();
 
-    return wrapDoc(MusicProfiles.findOne({ userId: Meteor.userId()}));
+Template.registerHelper( 'userId', () => {
+    event.preventDefault();
+    return Meteor.userId();
 });
 
-Template.registerHelper( 'accountDoc', () => {
+Template.registerHelper( 'getProfileRoute', (id = Meteor.userId()) =>{
+  return "profile/"+id
+})
+
+
+Template.registerHelper( 'profileDoc', (id = FlowRouter.getParam("postId")) => {
+    event.preventDefault();
+    if (!id) {
+      id = Meteor.userId;
+    }
+    return wrapDoc(MusicProfiles.findOne({ userId: id}));
+});
+
+Template.registerHelper( 'accountDoc', (id = FlowRouter.getParam("postId")) => {
   	event.preventDefault();
-    return wrapDoc(Accounts.findOne({ userId: Meteor.userId()}));
+    if (!id) {
+      id = Meteor.userId;
+    }
+    return wrapDoc(Accounts.findOne({ userId: id}));
 });
 
-Template.registerHelper( 'accompanistProfileDoc', () => {
+Template.registerHelper( 'accompanistProfileDoc', (id = FlowRouter.getParam("postId")) => {
     event.preventDefault();
-    return wrapDoc(AccompanistProfile.findOne({ userId: Meteor.userId()}));
+    if (!id) {
+      id = Meteor.userId();
+    }
+    return wrapDoc(AccompanistProfile.findOne({ userId: id}));
 });
 
 Template.registerHelper( 'musicCompetitionsDoc', () => {
@@ -79,9 +98,9 @@ Template.results.helpers({
 	// print this from the new page
   accompanists: ()=> {
 		var gl = Session.get('geo_location')
-		var sd = Session.get('start_date') 
-		var ed = Session.get('end_date') 
-	
+		var sd = Session.get('start_date')
+		var ed = Session.get('end_date')
+
 		// convert dates to dates that can be compared with Mongo schema
 		var new_sd = new Date(sd)
 		var new_ed = new Date(ed)
@@ -90,8 +109,8 @@ Template.results.helpers({
 		if (gl && sd && ed) {
 		  console.log("Searched")
       return AccompanistProfile.find({
-        mylocation: gl, 
-        startDate:  {$lte: new_sd, $lte: new_ed}, 
+        mylocation: gl,
+        startDate:  {$lte: new_sd, $lte: new_ed},
         endDate: {$gte: new_sd, $gte: new_ed}}).fetch()
     }
     	// return No results found if null!!!!!!!!
@@ -100,17 +119,17 @@ Template.results.helpers({
   }
 });
 
-// Events 
+// Events
 
 Template.search.events({
 	'submit form': function(){
 	    event.preventDefault();
-	   
+
 	    //Constants submitted from the Home search bar
 	   	const geo_location = event.target.geo_location.value
 	   	const start_date = event.target.start_date.value
 	   	const end_date = event.target.end_date.value
-	 
+
 	  	Session.set('geo_location', geo_location)
 	    Session.set('start_date', start_date)
 	  	Session.set('end_date', end_date)
