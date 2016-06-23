97ce6d142375511dc520d2f86b74e799930eefd3
James Kim <jakim929@gmail.com>
2016-06-23 16:30:25 -0400

    Renamed collections

---
+++ collections/accompanistProfiles.js
@@ -0,0 +1,137 @@
+import { Mongo } from 'meteor/mongo';
+
+export const AccompanistProfiles = new Mongo.Collection('accompanistProfiles');
+
+AccompanistProfileSchema = new SimpleSchema({
+		Id: {
+			type: String,
+			label: "Accompanist User ID",
+			autoform: {
+				type: "hidden"
+			},
+			autoValue: function(){
+        return this.userId
+			}
+		},
+
+		repertoire: {
+    	type: [String],
+    	label: "Repertoire"
+  	},
+
+  	charge: {
+  		type: Number,
+  		label: "Hourly Charge",
+  		autoform: {
+      		options: [
+	        	{label: "$20", value: 20},
+	        	{label: "$40", value: 40},
+	        	{label: "$60", value: 60}
+      		]
+   		}
+  	},
+
+  	working_hours: {
+  		type: [String],
+  		label: "Times you prefer working",
+  		autoform: {
+  			type: "select-checkbox-inline",
+  			options: [
+        	{label: "Morning (8am - 12pm)", value: "morning"},
+        	{label: "Afternoon (12pm - 6pm)", value: "afternoon"},
+        	{label: "Night (6pm - 11pm)", value: "night"}
+      		]
+   		}
+  	},
+
+   	working_days: {
+  		type: [String],
+  		//noselect:true,
+  		label: "Days you want to accompany",
+  		autoform: {
+  			type: "select-checkbox-inline",
+  			options: [
+        	{label: "Monday", value: "monday"},
+        	{label: "Tuesday", value: "tuesday"},
+        	{label: "Wednesday", value: "wednesday"},
+        	{label: "Thursday", value: "thursday"},
+        	{label: "Friday", value: "friday"},
+        	{label: "Saturday", value: "saturday"},
+        	{label: "Sunday", value: "sunday"},
+      		]
+   		}
+  	},
+
+  	session_location: {
+  		type: String,
+  		label: "Session's Location",
+  		autoform: {
+  			options: [
+        	{label: "My Place", value: "My Place"},
+        	{label: "Student's Place", value: "Student's Place"},
+        	{label: "Doesn't matter", value: "Doesn't matter"}
+      		]
+  		}
+  	},
+
+    startDate: {
+      type: Date,
+      label: "Start Date"
+    },
+
+    endDate: {
+      type: Date,
+      label: "End Date"
+    },
+
+    // make this show up only if the accomp said he's willing to give sessoins in his place
+    mylocation: {
+      type: String,
+      label: "mylocation"
+    },
+
+    geolocation:{
+      type: Object,
+      blackbox: true,
+      label: 'geolocation',
+      autoform: {
+        type: "hidden",
+        label: false
+      },
+      optional: true
+  },
+
+    loc: {
+      type: Object,
+      index: '2dsphere',
+      label: "Location",
+      //blackbox: true,
+      autoform: {
+      type: "hidden",
+      omit: true
+        },
+        optional: true
+      },
+
+      "loc.type": {
+        type: String,
+        allowedValues: ["Point"],
+        label: "Start location type"
+      },
+
+      "loc.coordinates": {
+        type: [Number],
+        //minCount: 2,
+        //maxCount: 2,
+        decimal: true,
+    },
+
+    one_liner: {
+      type: String,
+      label: "Describe yourself in one sentence",
+    },
+
+});
+
+
+AccompanistProfiles.attachSchema(AccompanistProfileSchema)
---
+++ collections/basicProfiles.js
@@ -0,0 +1,45 @@
+import { Mongo } from 'meteor/mongo';
+
+export const BasicProfiles = new Mongo.Collection('basicProfiles');
+
+PaymentMethodSchema = new SimpleSchema({
+  paymentType: {
+    type: String,
+    label: "Payment Type"
+  }
+});
+
+BasicProfileSchema = new SimpleSchema({
+  userId: {
+    type: String,
+    label: "User ID",
+    autoform: {
+      type: "hidden"
+    },
+    autoValue: function() {
+      return this.userId
+    }
+  },
+
+  name: {
+    type: String,
+    label: "Name"
+  },
+
+  birthDate : {
+    type: Date,
+    label: "Date of Birth"
+  },
+
+  phone: {
+    type: String,
+    label: "Phone Number"
+  },
+
+  affiliation: {
+    type: String,
+    label: "Affiliation"
+  }
+});
+
+BasicProfiles.attachSchema(BasicProfileSchema)
